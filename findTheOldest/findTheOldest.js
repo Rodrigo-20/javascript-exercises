1abcc92994507dbac64b805e7bab166e9a8b60b0
Rodrigo <ronico041012@gmail.com>
2020-09-12 13:37:15 -0300

    exercise completed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
 
+    const oldest=people.map(person=>{
+        if(!("yearOfDeath" in person)){
+            person.yearOfDeath=(new Date).getFullYear();
+            //console.log((new Date).getFullYear());
+        }
+        return person;
+    })
+                        .sort((a,b)=>a.yearOfDeath-a.yearOfBirth>b.yearOfDeath-b.yearOfBirth?-1:1);
+    return oldest[0];
 }
-
+/*findTheOldest(  [
+    {
+      name: 'Carly',
+      yearOfBirth: 2018,
+    },
+    {
+      name: 'Ray',
+      yearOfBirth: 1962,
+      yearOfDeath: 2011
+    },
+    {
+      name: 'Jane',
+      yearOfBirth: 1912,
+      yearOfDeath: 1941
+    },
+  ])*/
 module.exports = findTheOldest
